b4b637f24d1c333d7506e41d03e53b7e9bb6bfb9
Hoang Le <lxmhoang@gmail.com>
2022-12-12 12:58:22 +0700

    update url

--- frontend/udagram-frontend/src/environments/environment.prod.ts
+++ frontend/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://af50508ce266340e98b31389fbd0eba3-2068243300.us-west-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a27994412a98d48898c2cab1e549824c-869178976.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
